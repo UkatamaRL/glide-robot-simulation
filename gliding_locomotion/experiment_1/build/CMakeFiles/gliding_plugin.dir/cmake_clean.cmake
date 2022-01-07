2f14c4b7384fec489b5ccd4cb35d8cd6123297ac
UkatamaRL <49161817+UkatamaRL@users.noreply.github.com>
2022-01-07 13:49:41 +0900

    Add files via upload

---
+++ gliding_locomotion/experiment_1/build/CMakeFiles/gliding_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gliding_plugin.dir/gliding_plugin.cc.o"
+  "libgliding_plugin.pdb"
+  "libgliding_plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gliding_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gliding_locomotion/experiment_1/build/z_connect_test.py
@@ -0,0 +1,2 @@
+import subprocess
+subprocess.run(['./vel','-1'])
